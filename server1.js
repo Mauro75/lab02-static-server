63957dd6432b32df58dddf6ec85e09efa95917d7
CPC 2 <CPC 2>
2016-07-07 08:51:02 -0500

    Lab03 IP-PORT server con ip y puerto tomados del entorno

--- server1.js
+++ server1.js
@@ -1,6 +1,15 @@
 // http
 var http = require("http");
-// Crear un servidor básico
+// Obteniendo información del entorno
+// de ejecución con respecto al IP
+// y al puerto que debemos usar en 
+// nuestro server.
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP || '127.0.0.1';
+if(IP=='127.0.0.1'){
+    console.log("> ---- EJECUTANDOCE EN MODO LOCAL ----");
+}
+//Crear un servidor bàsico
 var server = http.createServer(function(req, res){
     // Armar la respuesta http
     // Armar un encabezado http
@@ -9,11 +18,11 @@ var server = http.createServer(function(req, res){
         "Server" : "ITGAM@4.2.4"
     });
     // Enviamos la respuesta
-    res.write("Hola Mundo");
+    res.write("Hola Mauro Leal");
     // Cerrar la conexion
     res.end();
 });
 // Poner a trabajar al server
-server.listen(3000,'127.0.0.1',function(){
-    console.log(">Server listening @http://localhost:3000...");
+server.listen(PORT,IP,function(){
+    console.log(`>Server listening @http://${IP}:${PORT}...`);
 });
